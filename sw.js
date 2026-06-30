f202c0b0741efda555b5d414e6f45f501692703c
Anderson Pelegrini <pelegrinianderson4@gmail.com>
2026-06-30 18:13:53 -0300

    Atualizacao 30/06/2026 18:13

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - FV WME Engenharia
-const CACHE = 'wme-fv-v46';
+const CACHE = 'wme-fv-v47';
 
 // Arquivos do app shell para cache offline
 const SHELL = ['./', './index.html', './icon.svg', './manifest.json'];
